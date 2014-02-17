fdc700f3187aaab8648e8e4dd76cd1a92090837d
riko.chen <248490686@qq.com>
2014-02-17 17:09:50 +0800

    Revert "第一期上传"
    This reverts commit 836423d7efb5cdb8b2eef7db414b83f8211a6b45.

--- app/code/local/Mec/Pointsaddon/Block/Adminhtml/Sales/Order/Totals/Points.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Mec_Pointsaddon_Block_Adminhtml_Sales_Order_Totals_Points extends Mage_Adminhtml_Block_Sales_Order_Totals_Item {
-
-    public function getStrong() {
-        return true;
-    }
-
-}
--- app/code/local/Mec/Pointsaddon/Model/Mysql4/Pointset.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Mec_Pointsaddon_Model_Mysql4_Pointset extends Mage_Core_Model_Mysql4_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init("pointsaddon/pointset", "id");
-    }
-}
